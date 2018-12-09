7567e014b9105b13d2002aa0fa49487825edb95b
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-12-09 19:24:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bot = new Discord.Client();
 var servers = {};
 
 bot.on("ready", function() {
+    bot.user.setGame("je suis un gros pd sucez moi : mon id discord : TheProGameur78#7427")
     console.log("Ready");
 });
 
