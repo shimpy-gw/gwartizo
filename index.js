01b58eecb0a5d40a46f48efbcc4627db6f87601d
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-12-09 19:37:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bot = new Discord.Client();
 var servers = {};
 
 bot.on("ready", function() {
-    bot.user.setGame("> Subscribe to Fortnite en France, Masa, Shimpy")
+    bot.user.setGame("> start singite.exe ")
     console.log("Ready");
 });
 
