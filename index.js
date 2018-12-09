aa8210df1a5b9f0cf3a4a6ca304b5351eb36359a
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-12-09 17:55:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-var servers = {};
-
 bot.on("ready", function() {
     bot.user.setStatus("dnd");
     console.log("Ready");
